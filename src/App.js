03eb1c86380b2b14b85efe3fe4d5ec3c9ce278ee
Ericson Willians <ericson.willians@concil.com.br>
2019-05-08 14:25:27 -0300

    Refactored to swagger.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	
+	return <SwaggerUI url="https://s3-sa-east-1.amazonaws.com/concilswagger/external_api.yaml" />;
 }
 
 export default App;
